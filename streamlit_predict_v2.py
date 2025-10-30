0a0066fa7186f91448e67721a72e18830f1aa0ba
lht-utp <3380846323@qq.com>
2025-10-30 09:17:48 +0800

    Update streamlit_predict_v2.py

--- streamlit_predict_v2.py
+++ streamlit_predict_v2.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 #使用侧边栏实现多页面显示效果
 with st.sidebar:
-    st.image('images/rigth_logo.png', width=100)
+    st.image('rigth_logo.png', width=100)
     st.title('请选择页面')
     page = st.selectbox("请选择页面", ["简介页面", "预测分类页面"], label_visibility='collapsed')
 
@@ -80,7 +80,8 @@ elif page == "预测分类页面":
 
     with col_logo:
         if not submitted:
-            st.image('images/rigth_logo.png', width=300)
+            st.image('rigth_logo.png', width=300)
         else:
-            st.image(f'images/{predict_result_species}.png', width=300)
+            st.image(f'{predict_result_species}.png', width=300)
+
 
